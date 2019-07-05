dbfa485a270db5a0303ca028de8575a90dc3138a
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-05 11:09:11 -0700

    add logic to display one or multiple specialties per doctor

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
        results.data.sort();
        console.log(results);
        for (let i = 0; i < results.data.length; i++) {
-         $('.condition').append(`<option value="${results.data[i].uid}">${results.data[i].name}</option>`)
+         $('.specialty').append(`<option value="${results.data[i].uid}">${results.data[i].name}</option>`)
        };
      },
 
@@ -26,7 +26,7 @@ $(document).ready(function() {
     console.log('click event fired');
     event.preventDefault();
     const docName = $('.doctor-name').val();
-    const specialty = $('.condition').val();
+    const specialty = $('.specialty').val();
 
     let doctorRequest = new DoctorRequest();
     let promise = doctorRequest.getDoctorInfo(docName, specialty);
@@ -34,6 +34,19 @@ $(document).ready(function() {
     promise.then(function (response) {
          let results = JSON.parse(response);
          console.log(results);
+
+         for(let i = 0; i < results.data.length; i++) {
+            console.log('number of specialties' + results.data[i].specialties.length);
+             $('.search-output').append(
+               `<div class="profile${i}">
+                  <h4>Name: ${results.data[i].profile.last_name}, ${results.data[i].profile.first_name}</h4>
+                  <h5 class="specialties">Specialties: </h5>
+                </div>`);
+              for(let j = 0; j < results.data[i].specialties.length; j++) {
+                $(`.search-output .profile${i} .specialties`).append(results.data[i].specialties[j].name + ', ')
+              }
+
+         }
        },
 
     function (error) {
@@ -44,3 +57,7 @@ $(document).ready(function() {
 
   });
 });
+
+function appendDoctor () {
+
+}
