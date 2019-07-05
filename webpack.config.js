ce19c2df0ccb7dfc0c2370e9de05465c8cba43b7
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-05 08:24:33 -0700

    install node packages

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Super Galactic Age Calculator',
+      title: 'Doctor Lookup',
       template: './src/index.html',
       inject: 'body'
     })
