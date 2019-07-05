b207a60285ed9c12bf4da113153a582d44c6a04e
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-05 10:32:45 -0700

    refactor conditions to specialties

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -2,7 +2,7 @@ export class DoctorRequest {
   getDoctorInfo(docName,condition) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=45.520645,-122.677452,100&skip=2&limit=10&user_key=f1ca4d79ccf5825a4f879f0f53844461`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&${specialty}&location=45.520645,-122.677452,100&skip=2&limit=10&user_key=f1ca4d79ccf5825a4f879f0f53844461`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,11 +17,11 @@ export class DoctorRequest {
   };
 };
 
-export class GetConditions {
-  getConditions() {
+export class GetSpecialties {
+  getSpecialties() {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=f1ca4d79ccf5825a4f879f0f53844461`;
+      let url = `https://api.betterdoctor.com/2016-03-01/specialties?user_key=f1ca4d79ccf5825a4f879f0f53844461`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 import $ from 'jquery';
-import { DoctorRequest,GetConditions } from './doctor-lookup.js';
+import { DoctorRequest, GetSpecialties } from './doctor-lookup.js';
 
 $(document).ready(function() {
   console.log('jquery enabled');
 
-  let populateConditions = new GetConditions();
-  let promise = populateConditions.getConditions();
+  let populateSpecialties = new GetSpecialties();
+  let promise = populateSpecialties.getSpecialties();
 
   promise.then(function (response) {
        let results = JSON.parse(response);
-       console.log(results);
        console.log(results.data.length);
+       results.data.sort();
+       console.log(results);
        for (let i = 0; i < results.data.length; i++) {
          $('.condition').append(`<option value="${results.data[i].uid}">${results.data[i].name}</option>`)
        };
@@ -25,10 +26,10 @@ $(document).ready(function() {
     console.log('click event fired');
     event.preventDefault();
     const docName = $('.doctor-name').val();
-    const condition = $('.condition').val();
+    const specialty = $('.condition').val();
 
     let doctorRequest = new DoctorRequest();
-    let promise = doctorRequest.getDoctorInfo(docName, condition);
+    let promise = doctorRequest.getDoctorInfo(docName, specialty);
 
     promise.then(function (response) {
          let results = JSON.parse(response);
