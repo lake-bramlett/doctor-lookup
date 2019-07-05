d0fb3b96601d5eca8665135f736bc4a161e79d9e
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-05 09:43:18 -0700

    refine search API to return results based on user inputted name and default location (Portland)

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -2,7 +2,7 @@ export class DoctorRequest {
   getDoctorInfo(docName,condition) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}?location=45.520645,-122.677452&user_key=${apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=45.520645,-122.677452,100&skip=2&limit=10&user_key=f1ca4d79ccf5825a4f879f0f53844461`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
