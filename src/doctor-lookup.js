4e24b82d0977623fa14b50192a02ae6318084e86
Lake Bramlett <lake.bramlett@gmail.com>
2019-07-05 11:35:06 -0700

    initial css styles

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,8 +1,25 @@
 export class DoctorRequest {
-  getDoctorInfo(docName,condition) {
+  getDoctorByName(docName) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&location=45.520645,-122.677452,100&skip=2&limit=10&user_key=f1ca4d79ccf5825a4f879f0f53844461`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        };
+      };
+
+      request.open('GET', url, true);
+      request.send();
+    });
+  };
+
+  getDoctorBySpecialty(specialty) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?specialties=${specialty}&location=45.520645,-122.677452,100&skip=2&limit=10&user_key=f1ca4d79ccf5825a4f879f0f53844461`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -35,3 +52,7 @@ export class GetSpecialties {
     });
   };
 };
+
+export function appendDoctor() {
+
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import { DoctorRequest, GetSpecialties } from './doctor-lookup.js';
+import './styles.css';
 
 $(document).ready(function() {
   console.log('jquery enabled');
@@ -22,14 +23,14 @@ $(document).ready(function() {
    });
 
 
-  $('button').click(function(event) {
+  $('.name-button').click(function(event) {
+    $('.search-output').empty();
     console.log('click event fired');
     event.preventDefault();
     const docName = $('.doctor-name').val();
-    const specialty = $('.specialty').val();
 
-    let doctorRequest = new DoctorRequest();
-    let promise = doctorRequest.getDoctorInfo(docName, specialty);
+    let doctorRequestByName = new DoctorRequest();
+    let promise = doctorRequestByName.getDoctorByName(docName);
 
     promise.then(function (response) {
          let results = JSON.parse(response);
@@ -38,26 +39,55 @@ $(document).ready(function() {
          for(let i = 0; i < results.data.length; i++) {
             console.log('number of specialties' + results.data[i].specialties.length);
              $('.search-output').append(
-               `<div class="profile${i}">
+               `<div class="profile doctor${i}">
+                  <img src="${results.data[i].profile.image_url}" />
                   <h4>Name: ${results.data[i].profile.last_name}, ${results.data[i].profile.first_name}</h4>
                   <h5 class="specialties">Specialties: </h5>
                 </div>`);
               for(let j = 0; j < results.data[i].specialties.length; j++) {
                 $(`.search-output .profile${i} .specialties`).append(results.data[i].specialties[j].name + ', ')
-              }
+              };
 
-         }
+         };
        },
 
     function (error) {
        console.log(error.message);
      });
 
+  });
 
+  $('.specialty-button').click(function(event) {
+    $('.search-output').empty();
+    console.log('click event fired');
+    event.preventDefault();
+    const specialty = $('.specialty').val();
+
+    let doctorRequestBySpeciality = new DoctorRequest();
+    let promise = doctorRequestBySpeciality.getDoctorByName(specialty);
+
+    promise.then(function (response) {
+         let results = JSON.parse(response);
+         console.log(results);
+
+         for(let i = 0; i < results.data.length; i++) {
+            console.log('number of specialties' + results.data[i].specialties.length);
+             $('.search-output').append(
+               `<div class="profile doctor${i}">
+                  <img src="${results.data[i].profile.image_url}" />
+                  <h4>Name: ${results.data[i].profile.last_name}, ${results.data[i].profile.first_name}</h4>
+                  <h5 class="specialties">Specialties: </h5>
+                </div>`);
+              for(let j = 0; j < results.data[i].specialties.length; j++) {
+                $(`.search-output .profile${i} .specialties`).append(results.data[i].specialties[j].name + ', ')
+              };
+
+         };
+       },
+
+    function (error) {
+       console.log(error.message);
+     });
 
   });
 });
-
-function appendDoctor () {
-
-}
